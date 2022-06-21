30f514c0894d825cd4d3f2e53e2337d72f392718
byGaurav <gagupta@rediffmail.com>
2022-06-21 15:07:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import streamlit as st
 def clean(txt):
     for line in txt:
-        st.text(txt)
+        if '301' in line:
+            st.write('\n')
+            line1 = line.split()   #Reading first line in line1
+            line=next(txt)
+            line = line.replace("F E", "FE")
+            line2 =line.split() #Reading Second line in line2
+            if len(line2)<12:
+                line1.insert(line1.index('301')+5,'')
+                for m in range(12-len(line2)):
+                    line2.append('')
+            # Starting adding rollnumber till name
+            st.write(line1[0])
+            st.write(',')
+            #f1.write(line1[1])  # Uncomment to write gender
+            #f1.write(',')   #Uncoment to write gender
 
 form = st.form(key='my-form')
 txt = form.text_area('Enter your Result')
