204374746fe9e3a9cc461fb2a68b2641863989c7
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-06-21 08:09:27 +0000

    textarea

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.write('Hello World')
-st.text_area(label, value="", height=None, max_chars=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False)
+st.text_area('Text to analyze', '''
+     It was the best of times, it was the worst of times, it was
+     the age of wisdom, it was the age of foolishness, it was
+     the epoch of belief, it was the epoch of incredulity, it
+     was the season of Light, it was the season of Darkness, it
+     was the spring of hope, it was the winter of despair, (...)
+     ''')
