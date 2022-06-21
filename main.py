47b5f1415aa0a0ea484c2686df27a0ca1b2ac67a
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-06-21 14:09:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 form = st.form(key='my-form')
-name = form.text_input('Enter your name')
+name = form.text_area('Enter your name')
 submit = form.form_submit_button('Submit')
 
 st.write('Press submit to have your name printed below')
