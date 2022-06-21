6f9c78327d2757635cb4da8827c7af31347e1a03
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-06-21 08:08:04 +0000

    textarea

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 import streamlit as st
 st.write('Hello World')
+st.text_area(label, value="", height=None, max_chars=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False)
