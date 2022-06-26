6fc9c2708162840e284051223516de94ad98ac82
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-06-26 06:45:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import streamlit as st
-def clean(txt):
-    with open("txt.txt","r") as f:
-        for line in f:
-            if '301' in line.split():
-                st.write('\n')
-                line1 = line.split()   #Reading first line in line1
-                line=next(f)
-                line = line.replace("F E", "FE")
-                line2 =line.split() #Reading Second line in line2
-                if len(line2)<12:
-                    line1.insert(line1.index('301')+5,'')
-                    for m in range(12-len(line2)):
-                        line2.append('')
-                # Starting adding rollnumber till name
-                st.write(line1[0])
-                st.write(',')
-                #f1.write(line1[1])  # Uncomment to write gender
-                #f1.write(',')   #Uncoment to write gender
-
+def clean(data_file):
+    for line in data_file:
+        st.write(line)
+   
 #form = st.form(key='my-form')
 #txt = form.text_area('Enter your Result')
 #submit = form.form_submit_button('Submit')
@@ -29,8 +14,8 @@ def clean(txt):
 
 
 
-txt = st.file_uploader("Add text file !")
-if txt:
-    clean(txt)
+data_file = st.file_uploader("Add text file !")
+if data_file:
+    clean(data_file)
     #for line in uploaded_file:
         #st.write(line)
