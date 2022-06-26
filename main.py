d022f78270ee2bafd4c08bd8831dd61ab10f24ad
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-06-26 07:08:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ def clean(data_file):
 
 
 
-data_file = st.file_uploader("Add text file !")
+data_file = st.file_uploader("Add text file !",type=["txt"])
 if data_file:
+    data_file = str(data_file.read(),"utf-8")
     clean(data_file)
     #for line in uploaded_file:
         #st.write(line)
