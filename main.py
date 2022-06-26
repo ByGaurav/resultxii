4a8ba015ed377fc9fc9eed14dd331d94ac6013eb
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-06-26 06:36:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import streamlit as st
 def clean(txt):
-    for line in txt.readlines():
-        if '301' in line.split():
-            st.write('\n')
-            line1 = line.split()   #Reading first line in line1
-            line=next(line)
-            line = line.replace("F E", "FE")
-            line2 =line.split() #Reading Second line in line2
-            if len(line2)<12:
-                line1.insert(line1.index('301')+5,'')
-                for m in range(12-len(line2)):
-                    line2.append('')
-            # Starting adding rollnumber till name
-            st.write(line1[0])
-            st.write(',')
-            #f1.write(line1[1])  # Uncomment to write gender
-            #f1.write(',')   #Uncoment to write gender
+    with open("txt","r") as f:
+        for line in f:
+            if '301' in line.split():
+                st.write('\n')
+                line1 = line.split()   #Reading first line in line1
+                line=next(f)
+                line = line.replace("F E", "FE")
+                line2 =line.split() #Reading Second line in line2
+                if len(line2)<12:
+                    line1.insert(line1.index('301')+5,'')
+                    for m in range(12-len(line2)):
+                        line2.append('')
+                # Starting adding rollnumber till name
+                st.write(line1[0])
+                st.write(',')
+                #f1.write(line1[1])  # Uncomment to write gender
+                #f1.write(',')   #Uncoment to write gender
 
 #form = st.form(key='my-form')
 #txt = form.text_area('Enter your Result')
@@ -28,8 +29,8 @@ def clean(txt):
 
 
 
-uploaded_file = st.file_uploader("Add text file !")
-if uploaded_file:
-    clean(uploaded_file)
+txt = st.file_uploader("Add text file !")
+if txt:
+    clean(txt)
     #for line in uploaded_file:
         #st.write(line)
