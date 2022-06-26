0a2becf53c80839ddbac8040c384b4f7820dd245
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-06-26 07:32:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import streamlit as st
-def clean(data_file):
+def clean(f):
     with open("out.txt", "w") as f1:
-        for line in data_file:
+        f1.write('R.No.'+','+'Name'+','+'SUB1'+','+'MRK1'+','+'GRD1'+','+\
+       'SUB2'+','+'MRK2'+','+'GRD2'+','+'SUB3'+','+'MRK3'+','+'GRD3'+','+\
+       'SUB4'+','+'MRK4'+','+'GRD4'+','+'SUB5'+','+'MRK5'+','+'GRD5'+','+\
+       'SUB6'+','+'MRK6'+','+'GRD6'+','+'we'+','+'hp'+','+'gs'+','+'Res')
+        for line in f:
             line = line.decode('ascii')
-            line=next(data_file)
+            line=next(f)
             st.write(line)
             break
    
