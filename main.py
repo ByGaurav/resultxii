21aa1dcf14d73fa2e5c5c553d4b2a664523b6be4
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-06-27 16:14:02 +0000

    df4

--- main.py
+++ main.py
@@ -79,11 +79,12 @@ def process_file():
     #Calculating Percentage
     df['Per'] = df.iloc[:,cList].apply(pd.to_numeric, errors='coerce').sum(axis=1)/5
     df['Per'] = df['Per'].round(decimals = 3)
+    df['Per'] = df['Per'].apply(str)
     df_original = df
     df_original['SUB6'] = df_original['SUB6'].fillna("")
     df_original['MRK6'] = df_original['MRK6'].fillna("")
     df_original['GRD6'] = df_original['GRD6'].fillna("")
-    st.dataframe(df_original.apply(str))
+    st.dataframe(df_original.astype(str))
     #st.dataframe(df_original.style.format({"Total": "{:.0f}","Per": "{:.2f}"}).astype(str))
     
     def analy(subject):
