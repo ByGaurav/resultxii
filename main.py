2a4a1897250c467e6269d59cd743aea650311531
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-06-27 04:40:26 +0000

    32

--- main.py
+++ main.py
@@ -80,9 +80,9 @@ def process_file():
     df['Per']= df.iloc[:,cList].apply(pd.to_numeric, errors='coerce').sum(axis=1)/5
     df_original = df
     df_original['SUB6'] = df_original['SUB6'].fillna("")
-    df_original['MRK6'] = df_original['SUB6'].fillna("")
-    df_original['GRD6'] = df_original['SUB6'].fillna("")
-    st.dataframe(df.style.format({"Total": "{:.0f}","Per": "{:.2f}"}))
+    df_original['MRK6'] = df_original['MRK6'].fillna("")
+    df_original['GRD6'] = df_original['GRD6'].fillna("")
+    st.dataframe(df_original.style.format({"Total": "{:.0f}","Per": "{:.2f}"}))
     
     def analy(subject):
         df1 = df[ (df.SUB1 == subject)][['Name', 'SUB1','MRK1','GRD1']]
