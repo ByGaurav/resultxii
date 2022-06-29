9b6b7daf10c341c1513d9a72d1bd0c8cf552f321
byGaurav <gagupta@rediffmail.com>
2022-06-29 22:35:48 +0530

    kj

--- main.py
+++ main.py
@@ -109,4 +109,4 @@ df_original = df
 df_original['SUB6'] = df_original['SUB6'].fillna("")
 df_original['MRK6'] = df_original['MRK6'].fillna("")
 df_original['GRD6'] = df_original['GRD6'].fillna("")
-st.dataframe(df_original.astype(str))}}}
+st.dataframe(df_original.astype(str))
