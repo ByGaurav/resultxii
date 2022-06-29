ab61293c8057b2ddc8e97c9810fcbb05861d9c9b
byGaurav <gagupta@rediffmail.com>
2022-06-29 22:23:26 +0530

    fg

--- main.py
+++ main.py
@@ -32,51 +32,51 @@ if not data_file:
 school_code = data_file.name[:-4]
 clean_file = school_code.astype(str)+".txt" 
 # Cleaning File
-  with open(clean_file, "w") as f1:
-      f1.write('R.No.'+','+'Name'+','+'SUB1'+','+'MRK1'+','+'GRD1'+','+\
-      'SUB2'+','+'MRK2'+','+'GRD2'+','+'SUB3'+','+'MRK3'+','+'GRD3'+','+\
-      'SUB4'+','+'MRK4'+','+'GRD4'+','+'SUB5'+','+'MRK5'+','+'GRD5'+','+\
-      'SUB6'+','+'MRK6'+','+'GRD6'+','+'we'+','+'hp'+','+'gs'+','+'Res')
-      for line in fractions:
-          line = line.decode('ascii')
-          if "301" in line:
-                f1.write("\n")
+with open(clean_file, "w") as f1:
+    f1.write('R.No.'+','+'Name'+','+'SUB1'+','+'MRK1'+','+'GRD1'+','+\
+    'SUB2'+','+'MRK2'+','+'GRD2'+','+'SUB3'+','+'MRK3'+','+'GRD3'+','+\
+    'SUB4'+','+'MRK4'+','+'GRD4'+','+'SUB5'+','+'MRK5'+','+'GRD5'+','+\
+    'SUB6'+','+'MRK6'+','+'GRD6'+','+'we'+','+'hp'+','+'gs'+','+'Res')
+    for line in fractions:
+        line = line.decode('ascii')
+        if "301" in line:
+              f1.write("\n")
 
-                line1 = line.split()   #Reading first line in line1
-                line=next(functools)
-                line = line.decode('ascii')
-                line = line.replace("F E", "E")
-                line2 =line.split() #Reading Second line in line2
-                if len(line2)<12:
-                    line1.insert(line1.index('301')+5,'')
-                    for m in range(12-len(line2)):
-                          line2.append('')
+              line1 = line.split()   #Reading first line in line1
+              line=next(functools)
+              line = line.decode('ascii')
+              line = line.replace("F E", "E")
+              line2 =line.split() #Reading Second line in line2
+              if len(line2)<12:
+                  line1.insert(line1.index('301')+5,'')
+                  for m in range(12-len(line2)):
+                        line2.append('')
 
-                # Starting adding rollnumber till name
-                f1.write(line1[0].strip())
-                f1.write(',')
-                #f1.write(line1[1])  # Uncomment to write gender
-                #f1.write(',')   #Uncoment to write gender
-
-
-                lock = line1.index('301')
-                for i in range (2,lock):
-                    f1.write(line1[i])
-                    if i < lock-1:
-                        f1.write(' ')
-                # finshed adding till name
-                j=0
-                for i in range(lock,len(line1)):
-                    f1.write(',')
-                    f1.write(line1[i])
-
-                    if len(line2)>j:
+              # Starting adding rollnumber till name
+              f1.write(line1[0].strip())
+              f1.write(',')
+              #f1.write(line1[1])  # Uncomment to write gender
+              #f1.write(',')   #Uncoment to write gender
 
 
-                        for k in range(j,j+2):
-                              f1.write(",")
-                              f1.write(line2[k].strip())
+              lock = line1.index('301')
+              for i in range (2,lock):
+                  f1.write(line1[i])
+                  if i < lock-1:
+                      f1.write(' ')
+              # finshed adding till name
+              j=0
+              for i in range(lock,len(line1)):
+                  f1.write(',')
+                  f1.write(line1[i])
 
-                        j=j+2
-  f1.close()
-  st.write(clean_file)
+                  if len(line2)>j:
+
+
+                      for k in range(j,j+2):
+                            f1.write(",")
+                            f1.write(line2[k].strip())
+
+                      j=j+2
+f1.close()
+st.write(clean_file)
