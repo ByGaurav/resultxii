fc02531234010ecfe9336e876a054967d9d5b337
byGaurav <32224654+ByGaurav@users.noreply.github.com>
2022-07-03 16:52:48 +0000

    mj

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ hide_st_style = """
 st.markdown(hide_st_style,unsafe_allow_html=True)
 user_input1, user_input2 = st.columns(2)
 with user_input1:
-  rno = st.text_input('Enter First Roll Number')))
+  rno = st.text_input('Enter First Roll Number')
+  if rno:
+    rno = int(float(rno))
 
 # Check Uplaoded File
 with user_input2:
